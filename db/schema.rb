ca909c96cc2b2c921c93c220b3e22a4f5822682d
Hikaru-q <132335482+Hikaru-q@users.noreply.github.com>
2023-06-06 11:38:32 +0000

    Book投稿時の評価項目追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_04_064617) do
+ActiveRecord::Schema.define(version: 2023_06_06_064639) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2023_06_04_064617) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "star"
   end
 
   create_table "favorites", force: :cascade do |t|

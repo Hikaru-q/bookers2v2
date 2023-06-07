5e9ae4d7958a54d455c112874238da0ec8aa21e1
Hikaru-q <132335482+Hikaru-q@users.noreply.github.com>
2023-06-07 10:40:32 +0000

    Book一覧ページのソート機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,9 +12,15 @@ class BooksController < ApplicationController
   end
 
   def index
-    to  = Time.current.at_end_of_day
-    from  = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorites).sort_by {|x| x.favorites.where(created_at: from...to).size}.reverse
+    if params[:latest]
+      @books = Book.latest
+    elsif params[:old]
+      @books = Book.old
+    elsif params[:star_count]
+      @books = Book.star_count
+    else
+      @books = Book.all
+    end
     @book = Book.new
   end
 

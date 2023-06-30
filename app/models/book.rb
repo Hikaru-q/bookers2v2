a742bd109f566a95b36e97bac3a73ef22cf9aa8d
Hikaru-q <132335482+Hikaru-q@users.noreply.github.com>
2023-06-30 03:35:22 +0000

    課題7b実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -17,6 +17,11 @@ class Book < ApplicationRecord
     favorites.where(user_id: user.id).exists?
   end
   
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } 
+    scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } 
+    scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } 
+    scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } 
+  
   def save_tags(savebook_tags)
     # 現在のユーザーの持っているskillを引っ張ってきている
     current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
